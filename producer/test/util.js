9eaad6431efd08018d1e794050ee0bc05addf1f4
K SUH <kangone@grana.com>
2018-11-19 01:47:51 +0800

    create-consumer: Refactor utility functions out of index.js and update tests accordingly

---
+++ producer/test/util.js
@@ -0,0 +1,54 @@
+const chai = require('chai');
+const assert = chai.assert;
+const { randAmount, randInterval, createTransaction } = require('../util');
+
+describe('util.js', function() {
+    describe('randAmount', function() {
+        it('should be a float', function() {
+            const n = randAmount(1, 10);
+            assert.isTrue(n % 1 !== 0);
+        });
+
+        it('should be between 1(inclusive) and 2(exclusive)', function() {
+            const n = randAmount(1, 2);
+            assert.isTrue(n >= 1);
+            assert.isTrue(n < 2);
+        });
+    });
+
+    describe('randInterval', function() {
+        it('should be an int', function() {
+            const n = randInterval(1, 10);
+            assert.isTrue(n % 1 === 0);
+        });
+
+        it('should be between 1(inclusive) and 2(exclusive)', function() {
+            const n = randAmount(1, 2);
+            assert.isTrue(n >= 1);
+            assert.isTrue(n < 2);
+        });
+    });
+
+    describe('createTransaction', function() {
+        const trans = createTransaction();
+        it('should have producer_id', function() {
+            assert.property(trans, 'producer_id');
+        });
+
+        it('should have userid', function() {
+            assert.property(trans, 'userid');
+        });
+
+        it('should have type', function() {
+            assert.property(trans, 'type');
+        });
+
+        it('should have amount', function() {
+            assert.property(trans, 'amount');
+        });
+
+        it('should have amount of type float', function() {
+            assert.isTrue(trans.amount % 1 !== 0);
+        });
+    });
+});
---
+++ producer/util.js
@@ -0,0 +1,49 @@
+const os = require('os');
+
+/**
+ * Generate a random amount between two integers
+ * @param  {int} min
+ * @param  {int} max
+ * @return {float}
+ */
+const randAmount = function(min, max) {
+    return parseFloat(((Math.random() * (max - min)) + min).toFixed(2));
+}
+
+/**
+ * Generate random interval
+ * @param  {int} min seconds
+ * @param  {int} max seconds
+ * @return {int} seconds
+ */
+const randInterval = function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const sleep = function(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Create a random transaction
+ * @return {object}
+ */
+const createTransaction = function() {
+    const types = ['payment', 'topup'];
+    const users = [1, 2, 3];
+    const amount = randAmount(1, 5001);
+
+    return {
+        producer_id: os.hostname(),
+        userid: users[Math.floor(Math.random() * users.length)],
+        type: types[Math.floor(Math.random() * types.length)],
+        amount: amount
+    };
+}
+
+module.exports = {
+    randAmount: randAmount,
+    randInterval: randInterval,
+    sleep: sleep,
+    createTransaction: createTransaction,
+};

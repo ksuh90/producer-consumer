dacf4e478c8e2320c21058996805a805392b283a
K SUH <kangone@grana.com>
2018-11-19 03:12:49 +0800

    create-consumer: Factor out util functions to separate file

--- producer/index.js
+++ producer/index.js
@@ -1,48 +1,6 @@
 const amqp = require('amqplib');
-const os = require('os');
-const redis = require('redis');
-const redisClient = redis.createClient(process.env.REDIS_URL);
+const { randAmount, randInterval, createTransaction, sleep } = require('./util');
 
-/**
- * Generate a random amount between two integers
- * @param  {int} min
- * @param  {int} max
- * @return {float}
- */
-const randAmount = function(min, max) {
-    return ((Math.random() * (max - min)) + min).toFixed(2);
-}
-
-/**
- * Generate random interval
- * @param  {int} min seconds
- * @param  {int} max seconds
- * @return {int} seconds
- */
-const randInterval = function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-/**
- * Create a random transaction
- * @return {object}
- */
-const createTransaction = function() {
-    const types = ['payment', 'topup'];
-    const users = [1, 2, 3];
-    const amount = randAmount(1, 5001);
-
-    return {
-        producerId: os.hostname(),
-        userid: users[Math.floor(Math.random() * users.length)],
-        type: types[Math.floor(Math.random() * types.length)],
-        amount: parseFloat(amount)
-    };
-}
-
-const sleep = function(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 /**
  * The producer
@@ -57,8 +15,8 @@ const produce = async function() {
         const payload = createTransaction();
         
         await channel.sendToQueue(process.env.QUEUE_NAME, Buffer.from(JSON.stringify(payload)));
-        console.log(' [x] Interval %s seconds', n.toString());
-        console.log(payload);
+        //console.log(' [x] Interval %s seconds', n.toString());
+        //console.log(payload);
         await sleep(n * 1000);
     }
 

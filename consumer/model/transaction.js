6045f8e06471e9707d1b94ff07604a42f8e0411c
K SUH <kangone@grana.com>
2018-11-18 12:47:30 +0800

    create-consumer: Create transaction model

---
+++ consumer/model/transaction.js
@@ -0,0 +1,39 @@
+/**
+ * Describes a transaction model
+ */
+class Transaction {
+    constructor(producerId, type, userid, amount, id) {
+        this._producerId = producerId;
+        this._type = type;
+        this._userid = userid;
+        this._amount = parseInt(amount);
+        this._id = id || null;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id){
+        this._id = id;
+    }
+
+    asJSON() {
+        let ret = {
+            producerId: this._producerId,
+            type: this._type,
+            userid: this._userid,
+            amount: this._amount
+        };
+        if (this._id) {
+            ret['id'] = this._id;
+        } 
+        return ret;
+    }
+}
+
+module.exports = Transaction;

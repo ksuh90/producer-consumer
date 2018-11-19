ae6da41e5006987768434cf666fdda7871e97bc4
K SUH <kangone@grana.com>
2018-11-20 03:34:08 +0800

    onoff: Add event listener for toggling the producer on/off. Run server for endpoint

--- producer/index.js
+++ producer/index.js
@@ -1,3 +1,32 @@
+const eventEmitter = require('events');
 const producer = require('./producer');
+const server = require('./server');
 
+// The event emitter for toggling producer.
+const ee = new eventEmitter();
+
+// Event listener for toggling the producer on/off
+ee.on('toggle', function (mode) {
+    let globalMode = parseInt(process.env.PRODUCER_MODE);
+    switch(mode) {
+        case 1:
+            console.log('toggle ON');
+            if (!globalMode) {
+                process.env.PRODUCER_MODE = 1;
+                producer.produce();
+            }
+            break;
+        default:
+            console.log('toggle OFF');
+            if (globalMode) {
+                process.env.PRODUCER_MODE = 0;
+            }
+            break;
+    }
+});
+
+const httpServer = server(ee);
+httpServer.listen(8125);
+
+// Start producing
 producer.produce();

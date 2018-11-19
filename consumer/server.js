40b71ba1dd7ff81e302f80d40aa7aa6e030e1863
K SUH <kangone@grana.com>
2018-11-20 02:43:19 +0800

    onoff: Remove unused dependency

--- consumer/server.js
+++ consumer/server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
 const server = http.createServer(function (request, response) {
     //console.log('request ', request.url);

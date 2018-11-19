d46fa75b04deb43f48733335d141b51787fcc299
K SUH <kangone@grana.com>
2018-11-20 03:33:02 +0800

    onoff: Use env MODE variable inside while loop

--- producer/producer.js
+++ producer/producer.js
@@ -5,14 +5,15 @@ const produce = async function() {
     const connection = await amqp.connect(process.env.MESSAGE_QUEUE);
     const channel = await connection.createChannel();
     await channel.assertQueue(process.env.QUEUE_NAME, { durable: false });
-    
-    while (1) {
+
+    while (parseInt(process.env.PRODUCER_MODE)) {
         const n = randInterval(1, 6);
         const payload = createTransaction();
+        await channel.sendToQueue(
+            process.env.QUEUE_NAME,
+            Buffer.from(JSON.stringify(payload))
+        );
         
-        await channel.sendToQueue(process.env.QUEUE_NAME, Buffer.from(JSON.stringify(payload)));
-        //console.log(' [x] Interval %s seconds', n.toString());
-        //console.log(payload);
         await sleep(n * 1000);
     }
 

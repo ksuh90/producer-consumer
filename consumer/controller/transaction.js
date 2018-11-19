01b7927c97525475081b66b549e1f7e0b9e6dad6
K SUH <kangone@grana.com>
2018-11-19 20:42:12 +0800

    web-ui: Make controller module as a class

--- consumer/controller/transaction.js
+++ consumer/controller/transaction.js
@@ -9,78 +9,81 @@ const hgetAsync = promisify(redisClient.hget).bind(redisClient);
 const hsetAsync = promisify(redisClient.hset).bind(redisClient);
 const hmsetAsync = promisify(redisClient.hmset).bind(redisClient);
 
-/**
- * Get a new transaction id by keeping track of an incremental number
- */
-const getNewTransId = async function() {
-    const uidKey = 'transaction:id';
-    let uid = await getAsync(uidKey);
-    const newId = uid ? parseInt(uid) + 1 : 1;
-    await setAsync(uidKey, newId);
-    return newId;
-}
 
 /**
- * Insert a transaction entry to database
- * @param  {Transaction} trans
- * @return {bool}
+ * Describes a controller for transaction
  */
-const insert = async function(trans) {
-    const key = 'transaction:' + trans.id;
-    return await hmsetAsync([
-        key,
-        'transaction_id', trans.id,
-        'producer_id', trans.producerId,
-        'type', trans.type,
-        'userid', trans.userid,
-        'amount', trans.amount
-    ]);
-}
-
-/**
- * Apply transaction to user's account
- * @param  {string} userid
- * @param  {string} type
- * @param  {float} amount
- * @return {bool}
- */
-const applyToAccount = async function(userid, type, amount) {
-    const key = 'user:' + userid;
-    let user = new userModel(
-        userid,
-        parseFloat(await hgetAsync(key, 'balance'))
-    ); 
-
-    switch (type) {
-        case 'payment':
-            user.subtractAmount(amount);
-            break;
-        case 'topup':
-            user.addAmount(amount);
-            break;
-        default:
-            break;
+class Transaction {
+    constructor(transObject) {
+        this._transModel = new transactionModel(
+            transObject.producer_id,
+            transObject.type,
+            transObject.userid,
+            transObject.amount
+        );
     }
-    return await hsetAsync(key, 'balance', user.balance);
+
+    /**
+     * Get a new transaction id by keeping track of an incremental number
+     */
+    async getNewTransId() {
+        const uidKey = 'transaction:id';
+        let uid = await getAsync(uidKey);
+        const newId = uid ? parseInt(uid) + 1 : 1;
+        await setAsync(uidKey, newId);
+        return newId;
+    }
+
+    /**
+     * Apply transaction to user's account
+     * @return {bool}
+     */
+    async applyToAccount() {
+        const key = 'user:' + this._transModel.userid;
+        let user = new userModel(
+            this._transModel.userid,
+            parseFloat(await hgetAsync(key, 'balance'))
+        ); 
+
+        switch (this._transModel.type) {
+            case 'payment':
+                user.subtractAmount(this._transModel.amount);
+                break;
+            case 'topup':
+                user.addAmount(this._transModel.amount);
+                break;
+            default:
+                break;
+        }
+        return await hsetAsync(key, 'balance', user.balance);
+    }
+
+    /**
+     * Insert a transaction entry to database
+     * @return {bool}
+     */
+    async insert() {
+        const key = 'transaction:' + this._transModel.id;
+        return await hmsetAsync([
+            key,
+            'transaction_id', this._transModel.id,
+            'producer_id', this._transModel.producerId,
+            'type', this._transModel.type,
+            'userid', this._transModel.userid,
+            'amount', this._transModel.amount
+        ]);
+    }
+
+    async execute() {
+        this._transModel.id = await this.getNewTransId();
+        // TODO
+        /*if (await this.applyToAccount()) {
+            await this.insert();
+        }*/
+
+        console.log(this._transModel.asJSON());
+        return this._transModel.asJSON();
+    }
 }
 
-const handler = async function(transObject) {
-    let trans = new transactionModel(
-        transObject.producer_id,
-        transObject.type,
-        transObject.userid,
-        transObject.amount,
-        await getNewTransId()
-    );
-
-    // TODO
-    /*if (await applyToAccount(trans.userid, trans.type, trans.amount)) {
-        await insert(trans);
-    }*/
-
-    console.log(trans.asJSON());
-}
-
-module.exports = {
-    handler: handler,
-};
+module.exports = Transaction;

863054764df25636cc4970f6db0615fc3edf0f92
K SUH <kangone@grana.com>
2018-11-20 04:11:26 +0800

    onoff: UI elements for toggling producers on/off

--- consumer/server.js
+++ consumer/server.js
@@ -6,16 +6,34 @@ const server = http.createServer(function (request, response) {
 
     var filePath = '.' + request.url;
     if (filePath == './') {
-        filePath = './index.html';
+        
     }
 
-    var contentType = 'text/html';
-
-    fs.readFile(filePath, function(error, content) {
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(content, 'utf-8');
-    });
-
+    switch(request.url.substring(1)) {
+        case '':
+        case 'index.html':
+            fs.readFile('./index.html', function(error, content) {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(content, 'utf-8');
+            });
+            break;
+        case 'on':
+            http.get('http://neat-back_producer_1:8125/on');
+            response.writeHead(200);
+            response.end();
+            break;
+        case 'off':
+            http.get('http://neat-back_producer_1:8125/off');
+            response.writeHead(200);
+            response.end();
+            break;
+        default:
+            response.writeHead(500);
+            response.end();
+            break;
+    }
 });
 
 module.exports = server;
+
+

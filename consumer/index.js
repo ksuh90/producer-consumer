7fa217a78dcb5d572914575401b1400ddc43c9b6
K SUH <kangone@grana.com>
2018-11-19 21:26:16 +0800

    web-ui: Create listener for consumed data and attach it to socket

--- consumer/index.js
+++ consumer/index.js
@@ -1,9 +1,10 @@
+const server = require('./server');
 const amqp = require('amqplib');
-const { handler } = require('./controller/transaction');
+const eventEmitter = require('events');
+const transController = require('./controller/transaction');
 
-const sleep = function(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+// The event emitter for transaction update for socket.io to listen in.
+const ee = new eventEmitter();
 
 /**
  * The consumer
@@ -15,16 +16,26 @@ const consume = async function() {
     await channel.prefetch(1);
 
     channel.consume(process.env.QUEUE_NAME, async (message) => {
-        await sleep(1000);
 
-        const content = message.content.toString('utf-8');
-
+        // Acknowledge
         channel.ack(message);
 
-        console.log(content);
-        await handler(JSON.parse(content));
-        //winston.info(`${task.message} received!`);
+        const data = JSON.parse(message.content.toString());
+        const controller = new transController(data);
+        const transaction = await controller.execute(data);
+        console.log(transaction);
+        ee.emit('transaction', transaction);
     });
 }
 
 consume();
+
+// Initialize socket.io
+const io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+    ee.on('transaction', function (data) {
+        socket.emit('transaction', data);
+    });
+});
+
+server.listen(8080);

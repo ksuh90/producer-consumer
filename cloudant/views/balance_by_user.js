c6c5cc61a6a00004f92b7738ae27a55574280cec
K SUH <kangone@grana.com>
2018-11-21 01:37:33 +0800

    use-couchdb: Remove unused file

--- cloudant/views/balance_by_user.js
+++
@@ -1,9 +0,0 @@
-function (doc) {
-    if (doc.doc_type == 'transaction') {
-        var amount = doc.amount;
-        if (doc.type == 'payment') {
-            amount *= -1;
-        }
-        emit(doc.userid, amount);
-    }
-}

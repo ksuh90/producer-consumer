e436d96eb1ee75807696a5716db5fe4fb0b225dd
K SUH <kangone@grana.com>
2018-11-21 03:22:22 +0800

    master: Add description

--- init-db.sh
+++ init-db.sh
@@ -1,9 +1,12 @@
 
 # Delete and create initial database
+echo "Deleting \"dev\" if already exists..."
 curl http://admin:pass@localhost:5984/dev -X DELETE
+echo "Creating new database \"dev\""
 curl http://admin:pass@localhost:5984/dev -X PUT
 
 # Dump initial db state
+echo "Dumping data into database"
 node_modules/.bin/couchrestore --url http://admin:pass@localhost:5984 --db dev < cloudant/transactions.txt
 
 exit 0
